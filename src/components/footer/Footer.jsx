0ccf549bb5a2e41a19daae10f72bbed8a3c45e9e
Oleksii <itechnologyshved@gmail.com>
2022-09-24 10:52:46 +0300

    add developer, subscribe, footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,97 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="col">
+          <h4>Products</h4>
+          <ul>
+            <li className="footer__list">
+              <a href="/">App</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Token Lists</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Defi</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4>Developers</h4>
+          <ul>
+            <li className="footer__list">
+              <a href="/">App</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Token Lists</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Defi</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4>Governance</h4>
+          <ul>
+            <li className="footer__list">
+              <a href="/">App</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Token Lists</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Defi</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4>Community</h4>
+          <ul>
+            <li className="footer__list">
+              <a href="/">App</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Token Lists</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Defi</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h4>About</h4>
+          <ul>
+            <li className="footer__list">
+              <a href="/">App</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Token Lists</a>
+            </li>
+            <li className="footer__list">
+              <a href="/">Defi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./Subscribe.css";
 
 function Subscribe() {
-  return <div>Subscribe</div>;
+  return (
+    <div className="subscribe">
+      <div className="content">
+        <h2>Join Our DeFi Community</h2>
+        <form
+          action="https://getform.io/f/75f3f925-ab0a-4b1b-9871-72b8baa243c3"
+          method="POST"
+        >
+          <div className="form__container display-col">
+            <input type="email" name="email" placeholder="Enter your email" />
+            <button className="btn">Sign Up</button>
+          </div>
+          <div className="form__container">
+            <input type="checkbox" />{" "}
+            <p>Yes, I agree to receive email communications from DeFi.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Subscribe;
